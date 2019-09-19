cca7e8107963bdbed4f7b8d018bfabd7ea43648e
Konstantin Kalinovsky <konstantin.kalinovsky@firma.seznam.cz>
2019-09-19 23:31:59 +0200

    Minor changes to easier work with docker hub

--- docker-push.sh
+++ docker-push.sh
@@ -3,5 +3,5 @@
 VERSION=$(grep "LABEL version" Dockerfile | cut -d'"' -f2)
 IMAGE_VERSION="norraxx/faf-exe-upload:$VERSION"
 
-docker build --rm=true --tag="$IMAGE_VERSION" .
+docker push "$IMAGE_VERSION"
 echo "Image build $IMAGE_VERSION"
--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 VERSION=$(grep "LABEL version" Dockerfile | cut -d'"' -f2)
-IMAGE_VERSION="faforever/faf-exe-upload:$VERSION"
+IMAGE_VERSION="norraxx/faf-exe-upload:$VERSION"
 
 docker stop faf-exe-upload
 docker rm faf-exe-upload
-docker run -d --restart=always --name faf-exe-upload -p 13667:13667 $IMAGE_VERSION
+docker run -d --restart=always --name faf-exe-upload -p 13667:13667 "$IMAGE_VERSION"
 echo "Container started $IMAGE_VERSION"
